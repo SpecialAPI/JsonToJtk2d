cb2598d5e069cbb7eb514ae3163765dc43e8570a
SpecialAPI <pavlov.andrei.d@gmail.com>
2022-08-29 20:25:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JsonToJtk2d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				string path;
+				if (args.Length != 0)
+				{
+					Console.WriteLine("Launch args given, getting directory from launch args");
+					path = args[0];
+				}
+				else
+				{
+					Console.WriteLine("Launch args not given, enter the target direcotry: ");
+					path = Console.ReadLine();
+				}
+				if (Directory.Exists(path))
+				{
+					Console.WriteLine("Directory found, processing files...");
+					string[] files = Directory.GetFiles(path, "*.json", SearchOption.AllDirectories);
+					Console.WriteLine("Found " + files.Length.ToString() + " files.");
+					Console.WriteLine("Starting the rename...");
+					foreach (string file in files)
+					{
+						File.Move(file, file.Substring(0, file.LastIndexOf(".")) + ".jtk2d");
+					}
+				}
+				else
+				{
+					Console.WriteLine("Directory not found, closing.");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			Console.WriteLine("Done!");
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# JsonToJtk2d
+ A simple program for .NET Framework 3.5 that renames .json files into .jtk2d files that are used by Mod the Gungeon API
